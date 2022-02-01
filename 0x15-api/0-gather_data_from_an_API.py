aa5f8e42fa4c3d2a44c60ecfd59b7680ceb9c1ff
SandraHoyos <slhoyosr@gmail.com>
2022-01-31 22:55:55 -0500

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 
 """
 Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list
+returns information about his/her TODO list.
 """
 
 import requests
