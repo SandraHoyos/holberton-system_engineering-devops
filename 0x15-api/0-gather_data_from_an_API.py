bea31fa69178203f4b6139dd5907e114994c2384
SandraHoyos <slhoyosr@gmail.com>
2022-01-31 23:08:31 -0500

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
+
 """
-using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list.
 """
+
 import requests
-from sys import argv
+import sys
 
+if __name__ == '__main__':
+    """Gets API endpoint, then identify a
+    user to display completed task info"""
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = sys.argv[1]
+    user = requests.get(url + 'users/{}'.format(userId)).json()
+    todo = requests.get(url + 'todos?userId={}'.format(userId)).json()
+    completed = []
 
-def GET_Api():
-    """ GET data from API"""
-    user_id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/'
-    # GET  METHOD
-    user = requests.get('{}users?id={}'.format(url, user_id)).json()[0]
-    request = requests.get('{}todos?userId={}'.format(url, user_id)).json()
-    name = user.get('name')
-    tasks = len(request)
-    # Method : List Comprehension
-    task_list = [todo.get("title") for todo in request
-                 if todo.get('completed')]
-    tasks_todo = len(task_list)
-    string = "Employee {} is done with tasks({}/{}):"
-    print(string.format(name, tasks_todo, tasks))
-    [print("\t {}".format(i)) for i in task_list]
-
-
-if __name__ == "__main__":
-    GET_Api()
+    for task in todo:
+        if task.get("completed"):
+            completed.append(task.get("title"))
+    print("Employee {} is done with task({}/{}):"
+          .format(user.get('name'), len(completed), len(todo)))
+    for task in completed:
+        print('\t', task)
