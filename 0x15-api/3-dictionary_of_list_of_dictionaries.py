0890e7e3df07262b0afea11ffbd42829a09b1225
SandraHoyos <slhoyosr@gmail.com>
2022-01-31 23:57:17 -0500

    create file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
+
 """Dictionary of list of dictionaries"""
 
 import json
 import requests
 
-if __name__ == '__main__':
-    endpoint = requests.get('https://jsonplaceholder.typicode.com/users')
-    users = endpoint.json()
-    endpoint = requests.get('https://jsonplaceholder.typicode.com/todos')
-    task = endpoint.json()
 
-    dic = {
-        str(data.get('id')): [
-            {
-                'username': data.get('username'),
-                'task': item .get('titles'), 'completed':
-                    item.get('completed')
-            }
-            for item in task
-            if item.get('userId') == data.get('id')
-        ]
-        for data in users
-    }
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(dic, json_file)
+if __name__ == "__main__":
+
+    """REST API url"""
+    url = "https://jsonplaceholder.typicode.com/"
+
+    users = requests.get(url + "users/").json()
+    tasks = requests.get(url + "todos/").json()
+
+    all_todos = {}
+    for user in users:
+        user_id = user.get("id")
+        task_list = [{"task": task.get('title'),
+                      "completed": task.get('completed'),
+                      "username": user.get("username")}
+                     for task in tasks if user_id == task.get("userId")]
+        all_todos[user_id] = task_list
+
+    with open("todo_all_employees.json", "w", newline="") as jsonfile:
+        json.dump(all_todos, jsonfile)
